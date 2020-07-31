52d387019b808981fce598a76d314ef438aafe1e
ShrikantVerma <43176895+ShrikantVerma@users.noreply.github.com>
2020-07-31 17:43:27 +0530

    Update service start.sh

--- service start.sh
+++ service start.sh
@@ -16,3 +16,34 @@ then
 else
    exit 0
 fi
+
+
+
+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+HOSTNAME=`hostname -i`
+NOW=$(date +"%Y-%m-%d")
+DATETIME=$(date +"%d-%m-%Y-%H:%M:%S")
+LOGFILE=/home/resolve/runningStatus.log
+output=UP
+/opt/resolve/bin/status.sh | awk -F':' '{print $1}' > component.txt
+
+cat component.txt | while read component
+do
+status=`/opt/resolve/bin/status.sh | grep $component | awk '{print $2}'`
+
+        if [ "$output" == "$status" ]
+ then
+        pid=`/opt/resolve/bin/status.sh | grep $component |  awk '{print $4}'`
+        #uptime=`ps -p $pid -o etimes | awk "NR==2"`
+        uptime=`ps -p $pid -o etimes | awk 'NR==2{print $1}'`
+
+                echo 'time:'$DATETIME' component_status:'$status' uptime:'$uptime' component:'$component' hostname:'$HOSTNAME'' >> $NOW.log
+else
+       if [ "$status" == "DOWN" ]; then
+
+           echo 'time:'$DATETIME' component_status:'$status' uptime:0 component:'$component' hostname:'$HOSTNAME'' >> $NOW.log
+        fi
+fi
+done
